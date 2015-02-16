472ad0d982dd451dc859d7c713200295485006b8
jnnk <jnnknnj@gmail.com>
2015-02-16 18:07:16 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name='pyrlp',
-    author='jnnk',
-    author_email='jnnknnj@gmail.com',
-    url='http://github.com/jnnk/pyrlp',
-    packages=['rlp'],
+    version='0.1-dev',
     description='A package for encoding and decoding data in and from '
                 'Recursive Length Prefix notation',
-    version='0.1-dev')
+    long_description="",
+    url='http://github.com/jnnk/pyrlp',
+    author='jnnk',
+    author_email='jnnknnj@gmail.com',
+    packages=['rlp'],
+    install_requires=[],
+)
