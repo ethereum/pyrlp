6edf08c050bf74ad564671fca12b7c1b8a17ab8b
jnnk <jnnknnj@gmail.com>
2015-02-27 15:36:25 +0100

    Splitted consume_item
    into consume_length_prefix and consume_payload

--- rlp/codec.py
+++ rlp/codec.py
@@ -65,43 +65,51 @@ def length_prefix(length, offset):
         raise ValueError('Length greater than 256**8')
 
 
-def consume_item(rlp, start=0):
-    """Read an item from an RLP string.
+def consume_length_prefix(rlp, start):
+    """Read a length prefix from an RLP string.
     
     :param rlp: the rlp string to read from
     :param start: the position at which to start reading
-    :returns: a tuple ``(item, end)``, where ``item`` is the read item and
-              ``end`` is the position of the first unprocessed byte
+    :returns: a tuple ``(type, length, end)``, where ``type`` is either ``str``
+              or ``list`` depending on the type of the following payload,
+              ``length`` is the length of the payload in bytes, and ``end`` is
+              the position of the first payload byte in the rlp string
     """
     b0 = ord(rlp[start])
     if b0 < 128:  # single byte
-        return (rlp[start], start + 1)
-    elif b0 < 128 + 56:
-        l = b0 - 128  # short string
-        return (rlp[start + 1:start + 1 + l], start + 1 + l)
+        return (str, 1, start)
+    elif b0 < 128 + 56:  # short string
+        return (str, b0 - 128, start + 1)
     elif b0 < 192:  # long string
         ll = b0 - 128 - 56 + 1
         l = big_endian_int.deserialize(rlp[start + 1:start + 1 + ll])
-        return (rlp[start + 1 + ll:start + 1 + ll + l], start + 1 + l + ll)
+        return (str, l, start + 1 + ll)
     elif b0 < 192 + 56:  # short list
-        end = start + 1 + b0 - 192
-        items = []
-        next_item_start = start + 1
-        while next_item_start < end:
-            item, next_item_start = consume_item(rlp, next_item_start)
-            items.append(item)
-        if next_item_start > end:
-            raise DecodingError('List length prefix announced a too small '
-                                'length', rlp)
-        return (items, next_item_start)
+        return (list, b0 - 192, start + 1)
     else: # long list
         ll = b0 - 192 - 56 + 1
         l = big_endian_int.deserialize(rlp[start + 1:start + 1 + ll])
         if l < 56:
             raise DecodingError('Long list prefix used for short list', rlp)
+        return (list, l, start + 1 + ll)
+
+
+def consume_payload(rlp, start, type_, length):
+    """Read the payload of an item from an RLP string.
+    
+    :param rlp: the rlp string to read from
+    :param type_: the type of the payload (``str`` or ``list``)
+    :param start: the position at which to start reading
+    :param length: the length of the payload in bytes
+    :returns: a tuple ``(item, end)``, where ``item`` is the read item and
+              ``end`` is the position of the first unprocessed byte
+    """
+    if type_ == str:
+        return (rlp[start:start + length], start + length)
+    elif type_ == list:
         items = []
-        next_item_start = start + 1 + ll
-        end = next_item_start + l
+        next_item_start = start
+        end = next_item_start + length
         while next_item_start < end:
             item, next_item_start = consume_item(rlp, next_item_start)
             items.append(item)
@@ -109,6 +117,20 @@ def consume_item(rlp, start=0):
             raise DecodingError('List length prefix announced a too small '
                                 'length', rlp)
         return (items, next_item_start)
+    else:
+        raise TypeError('Type must be either list or str')
+
+def consume_item(rlp, start):
+    """Read an item from an RLP string.
+    
+    :param rlp: the rlp string to read from
+    :param start: the position at which to start reading
+    :returns: a tuple ``(item, end)`` where ``item`` is the read item and
+              ``end`` is the position of the first unprocessed byte
+    """
+    t, l, s = consume_length_prefix(rlp, start)
+    item, end = consume_payload(rlp, s, t, l)
+    return (item, end)
 
 
 def decode(rlp, sedes=None, **kwargs):
@@ -124,7 +146,7 @@ def decode(rlp, sedes=None, **kwargs):
              root item
     """
     try:
-        item, end = consume_item(rlp)
+        item, end = consume_item(rlp, 0)
     except IndexError:
         raise DecodingError('RLP string to short', rlp)
     if end != len(rlp):
@@ -156,3 +178,13 @@ def infer_sedes(obj):
         return List(imap(infer_sedes, obj))
     msg = 'Did not find sedes handling type {}'.format(type(obj).__name__)
     raise TypeError(msg)
+
+
+#   class LazyRLP(object):
+
+#       def __init__(self, rlp):
+#           self.rlp = rlp
+#           self.decoded = None
+#           self.length = consume_length_prefix(rlp)
+
+#       def __getitem__(self, i):
