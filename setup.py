1d455169b4beba46b07d54ce93876e67be5993e5
Heiko Heiko <heiko@heiko.org>
2015-03-16 15:51:47 +0100

    add makefile

--- setup.py
+++ setup.py
@@ -1,14 +1,57 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
 
 setup(
     name='pyrlp',
     version='0.1-dev',
-    description='A package for encoding and decoding data in and from '
-                'Recursive Length Prefix notation',
-    long_description="",
-    url='http://github.com/jnnk/pyrlp',
-    author='jnnk',
+    description="A package for encoding and decoding data in and from Recursive Length Prefix notation",
+    long_description=readme + '\n\n' + history,
+    author="jnnk",
     author_email='jnnknnj@gmail.com',
-    packages=['rlp'],
-    install_requires=[],
+    url='https://github.com/ethereum/pyrlp',
+    packages=[
+        'rlp',
+    ],
+    package_dir={'rlp':
+                 'rlp'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='rlp ethereum',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
