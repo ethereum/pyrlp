78f402a2ef41d5286acd286b9b1c88b8d4f2d9ee
jnnk <jnnknnj@gmail.com>
2015-03-30 12:00:59 +0200

    Implemented rlp.peek

--- rlp/__init__.py
+++ rlp/__init__.py
@@ -2,5 +2,5 @@ from . import sedes
 from .codec import encode, decode, infer_sedes
 from .exceptions import RLPException, EncodingError, DecodingError,  \
                         SerializationError, DeserializationError
-from .lazy import decode_lazy, LazyList
+from .lazy import decode_lazy, peek, LazyList
 from .sedes import Serializable
