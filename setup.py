3d0ed37cc0d15407c488b6021317a31a8c6548ad
Heiko Heiko <heiko@heiko.org>
2015-09-26 10:19:43 +0200

    version bump

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='rlp',
-    version='0.3.10',
+    version='0.4.0',
     description="A package for encoding and decoding data in and from Recursive Length Prefix notation",
     long_description=readme,
     author="jnnk",
