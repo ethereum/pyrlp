17aba44a8bcd85c88b954aee5e2656333d820b54
Konrad Feldmeier <konrad@brainbot.com>
2016-06-09 09:48:07 +0200

    Bump version: 0.4.5 → 0.4.6
    Changelog:
    -  fix: rlp.utils.big_endian_to_int does not work with bytearray #21

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '0.4.5'
+version = '0.4.6'
 
 setup(
     name='rlp',
