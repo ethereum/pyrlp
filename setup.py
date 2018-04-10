618633343fa2b13744a75cee6bdcc18b3f841078
Piper Merriam <pipermerriam@gmail.com>
2018-04-10 12:20:59 -0600

    further changes to packaging

--- setup.py
+++ setup.py
@@ -60,5 +60,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
