9edbc38496113648427d2e2edd1430e6d8329839
Piper Merriam <pipermerriam@gmail.com>
2018-04-18 13:24:18 -0600

    Bump version: 1.0.0-beta.1 → 1.0.0-beta.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extras_require['dev'] = (
 setup(
     name='rlp',
     # *IMPORTANT*: Don't manually change the version here. See README for more.
-    version='1.0.0-beta.1',
+    version='1.0.0-beta.2',
     description="A package for Recursive Length Prefix encoding and decoding",
     long_description_markdown_filename='README.md',
     author="jnnk",
